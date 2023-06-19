96ad28cc63c4be627733c87047bc632e2b454b12
rahul20011252 <rahul.n.adhikari2001@gmail.com>
2023-06-19 16:52:27 +0530

    Utility Code Test

--- utility_code_test.c
+++ utility_code_test.c
@@ -19,39 +19,40 @@ int ascii_to_int(char *s)
   
   while(*s)
   {
-    if(*s =='\n' || *s == '\t' || *s == '\b' || *s ==' ' || !NUMBER(*s))
-    {
-      if(digit_started == 0 || sign_present == 0)
+      if(*s =='\n' || *s == '\t' || *s == '\b' || *s ==' ' || !NUMBER(*s))
       {
-        printf("Error: Invalid Character\n");
-        return -1;
+         if(digit_started == 0 || sign_present == 0)
+          {
+             printf("Error: Invalid Character\n");
+             return -1;
+          }
       }
-    }
-    else if(*s == PLUS || *s == MINUS)
-    {
-      if(digit_started)
+      else if(*s == PLUS || *s == MINUS)
+       {
+          if(digit_started)
+          {
+            printf("Error: Sign occured after the  Digit has Started\n");
+            return -1;
+          }
+          if(sign_present)
+          {
+            printf("Error: Sign Already Present\n");
+            return -1;
+          }
+      
+         sign_present = 1;
+         if(*s == PLUS)
+         sign = +1;
+         else if(*s == MINUS)
+         sign = -1;
+         s++;
+      }  
+      else if('0' <= (*s) <= '9')  
       {
-       printf("Error: Sign occured after the  Digit has Started\n");
-       return -1;
-      }
-      if(sign_present)
-      {
-       printf("Error: Sign Already Present\n");
-       return -1;
-      }
-      sign_present = 1;
-      if(*s == PLUS)
-       sign = +1;
-      else if(*s == MINUS)
-       sign = -1;
-      s++;
-    }  
-    else if('0' <= (*s) <= '9')  
-    {
-     digit_started = 1;
-     number = number * 10 + (*s - '0');
-     s++;
-    }
+         digit_started = 1;
+         number = number * 10 + (*s - '0');
+         s++;
+     }
   }
    number = number * sign;
   return number;
@@ -59,10 +60,10 @@ int ascii_to_int(char *s)
    
 int main()
 {
-  char ip_str[MAX_STR_SIZE];
-  printf("Enter the string\n");
-   scanf("%[^\n]",ip_str);
-  int value = ascii_to_int(ip_str);
-  printf("The number is %d\n",value);
-  return 0; 
+    char ip_str[MAX_STR_SIZE];
+    printf("Enter the string\n");
+      scanf("%[^\n]",ip_str);
+    int value = ascii_to_int(ip_str);
+    printf("The number is %d\n",value);
+    return 0; 
 }
