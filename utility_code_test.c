a981f18ed5f5f362c9934f04b147657327a210df
rahul20011252 <rahul.n.adhikari2001@gmail.com>
2023-06-21 09:28:09 +0530

    Utility Code Test

--- utility_code_test.c
+++ utility_code_test.c
@@ -1,10 +1,10 @@
 /* Thus file contains some utility functions which converts the string into ascii and 
 converts it to integers and prints it. It also validates wheather the entered character
-is valid or not*/
+is valid or not. Also convert the characters and strings to upper and lower characters. Concatenation of two strings using pointers is also performed.*/
 
 #include<stdio.h>
 
-#define MAX_STR_SIZE 10
+#define MAX_STR_SIZE 100
 
 #define MINUS '-'
 
@@ -12,10 +12,17 @@ is valid or not*/
 
 #define NUMBER(s) ('0' <= (s) <= '9')
 
+#define TO_LOWER(c) (((c)>='A' && (c)<='Z')?(c)=(c)+'a'-'A':(c))
+
+#define TO_UPPER(c) (((c)>='a' && (c)<='z')?(c)=(c)+'A'-'a':(c))
+
+char str1[MAX_STR_SIZE] = "Hello";
+char str2[MAX_STR_SIZE] = "World";
+
 int ascii_to_int(char *s)
 {
   int c,number = 0, digit_started = 0, sign_present = 0;
-  int sign = 1git ;
+  int sign = 1;
   
   while(*s)
   {
@@ -57,13 +64,129 @@ int ascii_to_int(char *s)
    number = number * sign;
   return number;
 }    
-   
+  
+int to_lower(int c)
+{
+  if(c>='A' && c<='Z')
+   return c = c + 'a' - 'A';
+  else
+   return c;
+}
+
+int to_upper(int c)
+{
+   if(c>='a' && c<='a')
+    return c = c + 'A' - 'a';
+   else
+    return c;
+}
+
+void string_to_lower(char s[])
+{
+  int c,i=0;
+  while(c=s[i])
+  {
+   TO_LOWER(c);
+    s[i]=c;
+   i++;
+  }
+}
+
+void string_to_upper(char s[])
+{
+ int c,i=0;
+ while(c=s[i])
+ {
+   TO_UPPER(c);
+    s[i]=c;
+   i++;
+ }
+}
+
+void str_copy(char *dest, char *src)
+{
+  while((*dest = *src)!= '\0')
+  {
+    dest++;
+    src++;
+  }
+}
+
+void mem_copy(char *dest, char *src, int size)
+{
+ while(size>0)
+ { 
+  *dest=*src;
+  dest++;
+  src++;
+  size--;
+ }
+}
+
+void print_memory(char *dest, int size)
+{
+ char c;
+ for(int i=0;i<size;i++)
+ {
+   c=*(dest+i);
+   printf("0x%x\n", (c&0xff));
+   if(i & 07 == 0x6)
+    printf("\n");
+ }
+ printf("\n");
+}
+  
+char *str_cat(char *str1,char *str2)
+{
+   char *s=str1;
+   if(!str1)
+   {
+     printf(" Error\n");
+     str1++;
+   }
+   else
+   {
+    while(*str1!='\0')
+     str1++;
+    while((*str1 = *str2)!='\0')
+     {
+      str2++;
+      str1++;
+     }
+     return s;
+   }
+}
+
 int main()
 {
-    char ip_str[MAX_STR_SIZE];
+    char ip_str[MAX_STR_SIZE],cp_str[MAX_STR_SIZE],m_str[MAX_STR_SIZE],c='Z',*cptr,*str;
+    
     printf("Enter the string\n");
       scanf("%[^\n]",ip_str);
+    
     int value = ascii_to_int(ip_str);
     printf("The number is %d\n",value);
+    
+    cptr=&c;
+    
+    printf("Lower Case (from macro) of Z is %c\n",TO_LOWER(*cptr));
+    printf("Upper case of a is %c\n",to_upper('a'));
+    
+    string_to_lower(ip_str);
+    printf(" String Lower case is %s\n",ip_str);
+    
+    string_to_upper(ip_str);
+    printf("String Upper case is %s\n", ip_str);
+    
+    str_copy(cp_str,ip_str);
+    printf("Copied String is %s\n",cp_str);
+    
+    mem_copy(m_str, ip_str,3);
+    printf("Copied Memory is %s\n",m_str);
+    print_memory(ip_str,8);
+    
+    str = str_cat(str1, str2);
+    printf(" String is %s\n", str);
+    
     return 0; 
 }
