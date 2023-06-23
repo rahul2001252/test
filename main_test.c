ff5c82e1975417a2c39720a044dfbdf829611fe6
rahul20011252 <rahul.n.adhikari2001@gmail.com>
2023-06-23 16:11:30 +0530

    Main Test

--- main_test.c
+++ main_test.c
@@ -4,13 +4,15 @@
 #include "state_transition_test.h"
 #include "character_input.h"
 #include "bit_manupulation.h"
+#include "va_list_test.h"
+#include "binary_search.h"
 
 int main()
 {
   int choice;
   do
   {
-   printf("Enter your choice (0 to exit)\n1. Utilities\n2. Character Input\n3. Bit Manupulation\n4. State Transition\n");
+   printf("Enter your choice (0 to exit)\n1. Utilities\n2. Character Input\n3. Bit Manupulation\n4. State Transition\n5. Variable Length arguements\n6. Binary Search\n");
    scanf("%d",&choice);
    switch(choice)
    {
@@ -21,6 +23,10 @@ int main()
     case 3 : bit_manupulation_main();
              break;
     case 4 : state_transition_main();
+             break;
+    case 5 : va_main();
+             break;
+    case 6:  binary_search_main();
              break;
    }
   }while(choice !=0);
